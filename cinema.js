fb7b721338cc5db84ed28796b3fb54ef1feb01b2
Shocktek <shocktek@hotmail.com>
2022-04-19 21:49:52 -0300

    O TAL DO GOMIDE

--- cinema.js
+++ cinema.js
@@ -1,1 +1,13 @@
 console.log('We made the files')        
+
+
+var catalogo = require("./catalogo.json")
+
+console.log(catalogo)
+var catalogoString = JSON.stringify(catalogo)
+
+
+console.log(catalogoString)
+
+var catalogoObj = JSON.parse(catalogoString)
+console.log(catalogoObj)
